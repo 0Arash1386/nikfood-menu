884f6f54354b1664321061088485cf4b648468a6
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-04 17:18:41 +0330

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https://rcumkcucyefyjatiqsfn.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_lXBVhpTDMjhzyAxRacfXAg_4TkRImHt";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
