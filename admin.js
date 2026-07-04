703b013cf2e65c21bdbb80f52e2d44bd207c2f90
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-04 18:51:23 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ async function loadFoods() {
     .order("id", { ascending: false });
 
   if (error) {
-    console.error(error);
+    alert(error.message);
     return;
   }
 
@@ -14,27 +14,56 @@ async function loadFoods() {
 
   data.forEach(food => {
     foods.innerHTML += `
-      <div style="border:1px solid #ddd;padding:15px;margin:10px;border-radius:10px">
+      <div class="food-card">
         <h3>${food.name}</h3>
-        <p>دسته: ${food.category}</p>
-        <p>قیمت: ${food.price}</p>
+        <p><b>دسته:</b> ${food.category}</p>
+        <p><b>قیمت:</b> ${food.price} تومان</p>
+
         <button onclick="deleteFood(${food.id})">🗑 حذف</button>
       </div>
     `;
   });
 }
 
-async function deleteFood(id) {
-  if (!confirm("حذف شود؟")) return;
+async function deleteFood(id){
+  if(!confirm("حذف شود؟")) return;
 
   await supabase
     .from("foods")
     .delete()
-    .eq("id", id);
+    .eq("id",id);
 
   loadFoods();
 }
 
+document.getElementById("save").onclick = async ()=>{
+
+  const name=document.getElementById("name").value;
+  const category=document.getElementById("category").value;
+  const price=document.getElementById("price").value;
+  const ingredients=document.getElementById("ingredients").value;
+
+  if(!name || !category || !price){
+    alert("همه فیلدها را پر کن");
+    return;
+  }
+
+  const {error}=await supabase
+  .from("foods")
+  .insert([{
+      name,
+      category,
+      price:Number(price),
+      ingredients
+  }]);
+
+  if(error){
+      alert(error.message);
+      return;
+  }
+
+  document.getElementById("name").value="";
+  document.getElementById("category
 document.getElementById("addFood").onclick = async () => {
 
   const name = prompt("نام غذا");
