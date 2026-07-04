94d60e52214687bf5d1bb8eb1582a834e9998cbf
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-04 11:59:37 +0330

    Create foods.js

---
+++ foods.js
@@ -0,0 +1,37 @@
+const foods = [
+{
+name:"نیک برگر",
+category:"burger",
+price:"0",
+description:"برگر مخصوص نیک فود",
+image:"assets/images/nikburger.jpg"
+},
+{
+name:"چیز برگر",
+category:"burger",
+price:"0",
+description:"برگر با پنیر چدار",
+image:"assets/images/cheeseburger.jpg"
+},
+{
+name:"فیله برگر",
+category:"burger",
+price:"0",
+description:"فیله مرغ سوخاری",
+image:"assets/images/fileburger.jpg"
+},
+{
+name:"پپرونی",
+category:"pizza",
+price:"0",
+description:"پیتزا پپرونی",
+image:"assets/images/pepperoni.jpg"
+},
+{
+name:"نیک پیتزا",
+category:"pizza",
+price:"0",
+description:"پیتزای مخصوص",
+image:"assets/images/nikpizza.jpg"
+}
+];
