0ba4c5f3d8db31d00f7fae5a8101b646b1b6cc81
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-05 11:52:31 +0330

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://rcumkcucyefyjatiqsfn.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_lXBVhpTDMjhzyAxRacfXAg_4TkRImHt";
-
-const { createClient } = supabase;
-
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
