6dc33c51caeee07ab33f2dd0b3e5b74a92472e81
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-05 10:53:06 +0330

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -2,4 +2,6 @@ const SUPABASE_URL = "https://rcumkcucyefyjatiqsfn.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_lXBVhpTDMjhzyAxRacfXAg_4TkRImHt";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const { createClient } = supabase;
+
+const db = createClient(SUPABASE_URL, SUPABASE_KEY);
