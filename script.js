b428b55e41390d248a5b88067e085d7c7c4e55a7
0Arash1386 <0arash.khaniki0@gmail.com>
2026-07-05 13:21:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const sliderImages = [
     "images/slide1.jpg",
     "images/slide2.jpg",
-    "images/slide3.jpg"
+    "images/slide3.jpg",
+    "images/slide4.jpg",
+    "images/slide5.jpg",
+    "images/slide6.jpg",
+    "images/slide7.jpg",
+    "images/slide8.jpg",
+    "images/slide9.jpg"
+    "images/slide10.jpg"
 ];
 
 // غذاها
